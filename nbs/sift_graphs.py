d7e32911e02f7404afa150e13cb8c046d2c12510
sal <sahmed9@buffalo.edu>
2019-09-14 01:07:27 -0400

    corner detector

--- nbs/sift_graphs.py
+++ nbs/sift_graphs.py
@@ -3,7 +3,7 @@ import os
 import cv2
 import numpy as np
 import matplotlib 
-
+import math
 from matplotlib import pyplot as plt
 
 def get_rois(img_dir, show=False):
@@ -36,11 +36,21 @@ def get_kp(channel, show=False):
             kp_filtered.append(k)
     kp_filtered, desc = sift.compute(channel, kp_filtered)    
     if show:
-        print(len(kp), len(kp_filtered))
+        print("kp vs filter", len(kp), len(kp_filtered))
 
     return kp, kp_filtered, desc
 
 def make_sift(img, show=False):
+    # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # gray = np.float32(gray)
+    # dst = cv2.cornerHarris(gray,2,3,0.04)
+    # img[dst>0.01*dst.max()]=[255,0,255]
+    # plt.imshow(img)
+    # plt.show()
+    # # # return(None)
+    # print(img)
+    # print(np.shape(img))
+
     im_grey = img.copy()
     kp = []; 
     kp_filtered = []; 
@@ -49,30 +59,31 @@ def make_sift(img, show=False):
     kpc2 = get_kp(img[:,:,1])
     kpc3 = get_kp(img[:,:,2])
     
-    for k in kpc1[0] : 
-        kp.append(k)
-    for k in kpc1[1] : 
-        kp_filtered.append(k)
-    for k in kpc1[2] : 
-        desc.append(k)
-    
-    for k in kpc2[0] : 
-        kp.append(k)
-    for k in kpc2[1] : 
-        kp_filtered.append(k)
-    for k in kpc2[2] : 
-        desc.append(k)
-    
-    for k in kpc3[0] : 
-        kp.append(k)
-    for k in kpc3[1] : 
-        kp_filtered.append(k)
-    for k in kpc3[2] : 
-        desc.append(k)
+    if kpc1[0]:
+        for k in kpc1[0] : 
+            kp.append(k)
+        for k in kpc1[1] : 
+            kp_filtered.append(k)
+        for k in kpc1[2] : 
+            desc.append(k)
+    if kpc2[0]:
+        for k in kpc2[0] : 
+            kp.append(k)
+        for k in kpc2[1] : 
+            kp_filtered.append(k)
+        for k in kpc2[2] : 
+            desc.append(k)
+    if kpc3[0]:
+        for k in kpc3[0] : 
+            kp.append(k)
+        for k in kpc3[1] : 
+            kp_filtered.append(k)
+        for k in kpc3[2] : 
+            desc.append(k)
 
-    print("ksp", np.shape(kp),"after filter",  np.shape(kp_filtered))
-    print("desc", np.shape(desc))
     if show:
+        print("ksp", np.shape(kp),"after filter",  np.shape(kp_filtered))
+        print("desc", np.shape(desc))
         tmp_img1 = cv2.drawKeypoints(im_grey, kp, im_grey)
         f, axarr = plt.subplots(2,1)
         axarr[0].imshow(tmp_img1) 
@@ -87,18 +98,13 @@ def generate_graph(nodes, img, show=True):
     edges = []
     for node in nodes :
         for node2 in nodes:
-            if node2 == node:
-                # self edges ??? 
+            if is_edge(nodes, node, node2):
+                if (node2,node) in edges : 
+                    pass
+                else:   
+                    edges.append((node, node2))
+            else:
                 pass
-            else: 
-                if is_edge(node, node2):
-
-                    if len(edges)>0 :
-                        # print(edges[-1], node2, node)
-                        if edges[-1][0]!=node2 and edges[-1][1]!= node:
-                            edges.append((node, node2))
-                    else:   
-                        edges.append((node, node2))
     
     if show:
         for node in nodes:
@@ -111,17 +117,23 @@ def generate_graph(nodes, img, show=True):
 
     return edges
 
-def is_edge(a, b, threshold=1):
-    dist =  np.linalg.norm(np.array(a)-np.array(b))
-    return True if dist<=threshold  else  False
+def is_edge(nodes, a, b, dthreshold=100, nthreshold=1):
+    # print(a, b)
+    node_dist =  np.linalg.norm(np.array(a)-np.array(b))
+    dist =  np.linalg.norm(np.array(nodes[a])-np.array(nodes[b]))
+    # dist =  math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 )
+    isit =  True if dist<=dthreshold and dist > 0 and node_dist<=nthreshold  else  False
+    # print("is edge", dist, isit)
+    return isit
     
 
 
 
 def main():
-    img_dir = "/home/sahmed9/Documents/reps/deep-text-recognition-benchmark/demo_image/"
+    # img_dir = "/home/sahmed9/Documents/reps/deep-text-recognition-benchmark/demo_image/"
     img_dir = "/home/sahmed9/Documents/data/kenny_data_share/images_png/aa/"
     imgs = get_rois(img_dir)
+    print("Imgs got  :", len(imgs))
     # kps = []
     # descs = []
     graphs = []
